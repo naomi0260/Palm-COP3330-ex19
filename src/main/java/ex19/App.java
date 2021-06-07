fb2b468b33718a2230752d2443a08278e7795189
naomi0260 <84354219+naomi0260@users.noreply.github.com>
2021-06-07 16:41:37 -0400

    Update App.java

--- src/main/java/ex19/App.java
+++ src/main/java/ex19/App.java
@@ -3,20 +3,8 @@ package ex19;
 import java.util.Scanner;
 
 /*
-Create a program to calculate the body mass index (BMI) for a person using the person’s height in inches and weight in pounds. The program should prompt the user for weight and height.
-
-Calculate the BMI by using the following formula:
-
-bmi = (weight / (height × height)) * 703
-If the BMI is between 18.5 and 25, display that the person is at a normal weight. If they are out of that range, tell them if they are underweight or overweight and tell them to consult their doctor.
-
-Example Output
-Your BMI is 19.5.
-You are within the ideal weight range.
-or
-
-Your BMI is 32.5.
-You are overweight. You should see your doctor.
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Naomi Palm
  */
 
 
